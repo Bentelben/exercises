0567aab84596a6c9e82f7eb9821bee3b90f5b520
Bentelben <60937127+Bentelben@users.noreply.github.com>
2021-04-15 22:23:58 +0300

    Create 15_04_2021.py

---
+++ 15_04_2021.py
@@ -0,0 +1,32 @@
+s = 1
+h = 1
+for i in range(0, 9):
+    h *= 3
+    s += 1/h
+
+print("5.34: " + str(s))
+
+n = int(input("5.35 Enter N: "))
+s = 1
+h = 1
+for i in range(1, n):
+    h += 1
+    s += ((i%2)*-2 + 1)*(1/h)
+
+print("5.35: " + str(s))
+
+print("5.61 Enter Nums: ")
+n = 0
+s = []
+while(True):
+    h = input()
+    if h=="":
+        print("Next class: ")
+        break
+    n += 1
+    s.append(int(h))
+h = []
+for i in range(0, n):
+    h.append(int(input()))
+
+print("5.56 First: " + str(sum(s)//len(s)) + " Second: " + str(sum(h)//len(h)))
